14ea2507a31f4372cfa74f7aeb9f2d2ac67592c7
hunao3o <siamm0528@gmail.com>
2022-10-22 01:58:32 +0900

    spring initalize

---
+++ src/main/java/com/crew92/ygd/api/YgdApiApplication.java
@@ -0,0 +1,13 @@
+package com.crew92.ygd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YgdApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crew92/ygd/api/YgdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crew92.ygd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
