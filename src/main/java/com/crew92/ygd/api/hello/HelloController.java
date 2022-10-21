5c8d18b65b3c40f748c03b70154df4ed2e53f8d2
hunao3o <siamm0528@gmail.com>
2022-10-22 02:14:16 +0900

    add HelloController

---
+++ src/main/java/com/crew92/ygd/api/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.crew92.ygd.api.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
