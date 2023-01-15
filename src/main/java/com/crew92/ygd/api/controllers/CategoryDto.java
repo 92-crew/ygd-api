9fd9dd2487ccd8dd6fed064e7c646d2c078a8c02
hunao3o <siamm0528@gmail.com>
2023-01-16 00:37:41 +0900

    dto 작성

---
+++ src/main/java/com/crew92/ygd/api/controllers/CategoryDto.java
@@ -0,0 +1,11 @@
+package com.crew92.ygd.api.controllers;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CategoryDto {
+    private final int id;
+    private final String name;
+}
---
+++ src/main/java/com/crew92/ygd/api/controllers/CodeDto.java
@@ -0,0 +1,11 @@
+package com.crew92.ygd.api.controllers;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CodeDto {
+    private final String code;
+    private final String name;
+}
